ea2d3437d15275ac55c75833024f6b169e84a4a2
anders-kolstad <anders-lk@hotmail.com>
2025-12-08 11:30:54 +0100

    fix UTF8 tag

---
+++ helpers.R
@@ -0,0 +1,23 @@
+get_root_NINA <- function(server = "P") {
+  server <- toupper(server)
+  if (!server %in% c("P", "R")) {
+    stop("server must be 'P' or 'R'")
+  }
+  if (.Platform$OS.type == "windows") {
+    base <- switch(server,
+                   P = "P:/",
+                   R = "R:/")
+  } else {
+    base <- switch(server,
+                   P = "/data/P-Prosjekter2/",
+                   R = "/data/R/")
+  } 
+  return(base)
+}
+
+
+fix_utf8_tag <- function(x) {
+  x2 <- x
+  Encoding(x2) <- "UTF-8"
+  x2
+}
